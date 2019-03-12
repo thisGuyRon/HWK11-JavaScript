e1a8166668b9e0ebe6dcd47f09f00de76a98ca3a
thisGuyRon <42878005+thisGuyRon@users.noreply.github.com>
2019-03-12 19:16:25 -0400

    commit 1, 3-12

---
+++ static/js/app.js
@@ -0,0 +1,47 @@
+// from data.js
+// data load (json)
+var tableData = data;
+// html controls to read
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+
+// initial load of data
+data.forEach(function(ufoData){
+    var ufoRow = tbody.append("tr");
+
+    Object.entries(ufoData).forEach(function([key, value]){
+        var ufoCell = ufoRow.append("td");
+        ufoCell.text(value);
+
+    })
+})
+// event listener
+button.on("click", handleClick);
+
+// function to handle button click
+function handleClick(){
+    // prevent console and page reload
+    d3.event.preventDefault();
+    // clear existing records
+    tbody.html("");
+
+    // function to grab filtered data
+    var inputElement=d3.select("#datetime");
+    var inputDate=inputElement.property("value");
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputDate);
+    console.log(filteredData);
+
+    // load filtered data
+    filteredData.forEach(function(fData){
+        var ufoRow = tbody.append("tr");
+    
+        Object.entries(fData).forEach(function([key, value]){
+            var fCell = tbody.append("td");
+            fCell.text(value);
+    
+        })
+    })
+
+   
+}
